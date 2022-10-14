546eb474c73e8ffb7b14db3bea86f6c98e97c427
Ali-james <alijamesfbs@gmail.com>
2022-10-13 22:23:03 -0700

    am stressed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main --Entry
- * Return: Always (0)
- * int main(void)
+ * main -Entry there is no much comment to make
+ * Return: Always 0
  */
+int main(void)
  {
 	 printf("with proper grammar, but the outcome is a piece of art,\n");
  Return (0);
