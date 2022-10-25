bf516b30facf053e003ce677c346994fc4be3ef8
Ali-james <alijamesfbs@gmail.com>
2022-10-24 22:42:16 -0700

    string reverse in c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,14 +13,15 @@ void print_rev(char *s)
 	int o;
 
 	while (*s != '\0')
+	{
 		longi++;
-	s++;
-}
-s--;
-for (o = longi; o > 0; o--)
-{
-	_putchar(*s);
+		s++;
+	}
 	s--;
-}
-_putchar('\n');
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 }
